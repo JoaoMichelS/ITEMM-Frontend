98edb296f6bf61b2cd0055fd35ea582a26bd67d6
andressabragavr <andressabragavr@gmail.com>
2023-11-13 16:54:16 -0300

    Grafico

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const Stack = createStackNavigator();
 function App(){
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MeusChamados">
+      <Stack.Navigator initialRouteName="MainOperador">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="NewUser" component={NewUserScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
